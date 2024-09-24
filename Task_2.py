e1ee7921b7941f0b332e5f0b2ef837cd15c99e13
ggpillow <ketnut3336@gmail.com>
2024-09-24 11:27:38 +1100

    Code optimization

--- Task_2.py
+++ Task_2.py
@@ -2,13 +2,12 @@ array_boys = ["Martin", "Nikolay", "Denis", "Peter", "Kail", "Pol", "Artur"]
 array_girls = ["Anna", "Nari", "Mary", "Olga", "Lena", "Rianna", "Beyonce"]
 count = 0
 
+
 if len(array_boys) == len(array_girls):
     array_boys.sort()
     array_girls.sort()
 
-    for partner_boy in range(len(array_boys)):
-        for partner_girl in range(1):
-            print(array_boys[partner_boy], "и", array_girls[partner_girl + count])
-            count += 1
+    for boy, girl in zip(array_boys, array_girls):
+        print(boy, "и", girl)
 else:
     print("Внимание, кто-то может остаться без пары.")
