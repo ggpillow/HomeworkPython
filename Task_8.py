716dc51ef5bf3ee9a66dffe4c1d528e21757ab41
ggpillow <ketnut3336@gmail.com>
2024-12-25 04:02:59 +1100

    changed code

--- Task_8.py
+++ Task_8.py
@@ -2,38 +2,41 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import requests
 from urllib.parse import quote
 
-oauth_token = "OAuth y0__wgBEMSW3vwDGNuWAyD_k4nsETLcNnt-nfbtkCOxAhM2OqIGWgfg"
-
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    oauth_token = None
+    image_url = None
+
     def do_GET(self):
-        file_paths = ["Uploads/Sun1.jpg"]
-        file_urls = ["https://new-science.ru/wp-content/uploads/2019/11/514.jpg"]
+        file_path = "Uploads/UserImage.jpg"
         html_response = "<html><head><meta charset='utf-8'></head><body>"
+
         
-        for file_path, file_url in zip(file_paths, file_urls):
-            response = requests.get(
-                f"https://cloud-api.yandex.net/v1/disk/resources?path={quote(file_path)}",
-                headers={"Authorization": oauth_token}
-            )
+        response = requests.get(
+            f"https://cloud-api.yandex.net/v1/disk/resources?path={quote(file_path)}",
+            headers={"Authorization": self.oauth_token}
+        )
+
+        if response.status_code == 200:
+            html_response += f"<div style='background-color: rgba(0, 200, 0, 0.25);'>{file_path} - Файл уже загружен.</div>"
+        else:
             
-            if response.status_code == 200:
-                html_response += f"<div style='background-color: rgba(0, 200, 0, 0.25);'>{file_path} - Файл загружен.</div>"
-            else:
-                upload_link_response = requests.get(
-                    f"https://cloud-api.yandex.net/v1/disk/resources/upload?path={quote(file_path)}&overwrite=true",
-                    headers={"Authorization": oauth_token}
-                )
-                if upload_link_response.status_code == 200:
-                    upload_link = upload_link_response.json().get('href')
-                    upload_file_response = requests.put(upload_link, data=requests.get(file_url).content)
-                    if upload_file_response.status_code == 201:
-                        html_response += f"<div style='background-color: rgba(0, 200, 0, 0.25);'>{file_path} - Файл успешно загружен.</div>"
-                    else:
-                        html_response += f"<div>{file_path} - Ошибка загрузки файла.</div>"
+            upload_link_response = requests.get(
+                f"https://cloud-api.yandex.net/v1/disk/resources/upload?path={quote(file_path)}&overwrite=true",
+                headers={"Authorization": self.oauth_token}
+            )
+            if upload_link_response.status_code == 200:
+                upload_link = upload_link_response.json().get('href')
+
+                
+                upload_file_response = requests.put(upload_link, data=requests.get(self.image_url).content)
+                if upload_file_response.status_code == 201:
+                    html_response += f"<div style='background-color: rgba(0, 200, 0, 0.25);'>{file_path} - Файл успешно загружен.</div>"
                 else:
-                    html_response += f"<div>{file_path} - Не удалось получить ссылку для загрузки.</div>"
-        
+                    html_response += f"<div>{file_path} - Ошибка загрузки файла.</div>"
+            else:
+                html_response += f"<div>{file_path} - Не удалось получить ссылку для загрузки.</div>"
+
         html_response += "</body></html>"
         self.send_response(200)
         self.send_header('Content-type', 'text/html; charset=utf-8')
@@ -42,6 +45,14 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
 
 def run_server():
+    
+    oauth_token = input("Введите OAuth токен Яндекс.Диска: ")
+    SimpleHTTPRequestHandler.oauth_token = f"OAuth {oauth_token}"
+
+    
+    image_url = input("Введите ссылку на изображение: ")
+    SimpleHTTPRequestHandler.image_url = image_url
+
     server_address = ('', 8000)
     httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
     print('Сервер запущен на порту 8000...')
